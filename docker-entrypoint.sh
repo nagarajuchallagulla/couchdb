c9c1c4f22177fccce3940e3265c08a6cef964558
Tobbi <tomas.bouda@gmail.com>
2015-11-16 21:15:23 +0100

    admin account - fix setup

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 # setup admin account
-[ "$ADMIN_USERNAME" ] && [ "$ADMIN_PASSWORD" ] && \
-    sed -i -r 's/;admin = mysecretpassword/'$ADMIN_USERNAME' = '$ADMIN_PASSWORD'/' /etc/couchdb/local.ini
+[ ! -f /etc/couchdb/.local_admin_created ] && [ "$ADMIN_USERNAME" ] && [ "$ADMIN_PASSWORD" ] && \
+    sed -i -r 's/;admin = mysecretpassword/'$ADMIN_USERNAME' = '$ADMIN_PASSWORD'/' /etc/couchdb/local.ini && \
+    touch /etc/couchdb/.local_admin_created &&
+    echo "Apache CouchDB admin account succesfully created"
 
 # run CouchDB server
 couchdb
