c9d84a47b5489c8cbd8565ea9e92c74eef01aae1
Tobbi <tomas.bouda@gmail.com>
2015-11-28 19:35:11 +0100

    add configuration: compactions, delayed_commits

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 # setup admin account
-[ ! -f /etc/couchdb/.local_admin_created ] && [ "$ADMIN_USERNAME" ] && [ "$ADMIN_PASSWORD" ] && \
+[ ! -f /etc/couchdb/.local_ini_configured ] && [ "$ADMIN_USERNAME" ] && [ "$ADMIN_PASSWORD" ] && \
     sed -i -r 's/;admin = mysecretpassword/'$ADMIN_USERNAME' = '$ADMIN_PASSWORD'/' /etc/couchdb/local.ini && \
-    touch /etc/couchdb/.local_admin_created &&
     echo "Apache CouchDB admin account succesfully created"
 
-# setup default compactions
-[ ! -f /etc/couchdb/.local_compactions_setup ] && [ "$COMPACTIONS_DEFAULT" ] && \
-    echo -e '\n[compactions]\n_default = '$COMPACTIONS_DEFAULT'\n' >> /etc/couchdb/local.ini && \
-    touch /etc/couchdb/.local_compactions_setup &&
-    echo "Apache CouchDB compactions succesfully set up"
+# setup configuration (delayed_commits, compations)
+[ ! -f /etc/couchdb/.local_ini_configured ] && \
+    sed -i -r 's/\[couchdb\]/\[couchdb\]\ndelayed_commits = false/' /etc/couchdb/local.ini && \
+    echo -e '\n[compactions]\n_default = [{db_fragmentation, "50%"}, {view_fragmentation, "60%"}, {from, "21:00"}, {to, "22:59"}]\n' >> /etc/couchdb/local.ini && \
+    echo "Apache CouchDB configuration succesfully set up"
+
+touch /etc/couchdb/.local_ini_configured
 
 # run CouchDB server
 couchdb
