6bd217e5696fb50600e92bfb69307ad05fa00755
Tobbi <tomas.bouda@gmail.com>
2015-12-06 20:30:11 +0100

    add admin-only configuration

--- docker-command.sh
+++ docker-command.sh
@@ -3,10 +3,15 @@
 SHARED=/usr/local/var/etc/couchdb/
 COUCHDB=/usr/local/etc/couchdb/
 
-# recover configuration
+# configuration
 
 [ -f $SHARED/local.ini ] && [[ $(diff $SHARED/local.ini $COUCHDB/local.ini) ]] && \
-    cp $SHARED/local.ini $COUCHDB/local.ini && echo "Apache CouchDB configuration succesfully set up"
+    cp $SHARED/local.ini $COUCHDB/local.ini && \
+    echo "Apache CouchDB => configuration successfully set up"
+
+[ "$ADMIN_USERNAME" ] && [ "$ADMIN_PASSWORD" ] && \
+    sed -i -r 's/;admin = mysecretpassword/'$ADMIN_USERNAME' = '$ADMIN_PASSWORD'/' $COUCHDB/local.ini && \
+    echo "Apache CouchDB => admin account successfully created"
 
 # run CouchDB server
 
