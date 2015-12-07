97163ad269eef4576371fb6e1146c5808f7b4f90
Tobbi <tomas.bouda@gmail.com>
2015-12-07 20:32:46 +0100

    fix shell variables

--- docker-command.sh
+++ docker-command.sh
@@ -9,8 +9,8 @@ COUCHDB=/usr/local/etc/couchdb/
     cp $SHARED/local.ini $COUCHDB/local.ini && \
     echo "Apache CouchDB => configuration successfully set up"
 
-[ "$ADMIN_USERNAME" ] && [ "$ADMIN_PASSWORD" ] && \
-    sed -i -r 's/;admin = mysecretpassword/'$ADMIN_USERNAME' = '$ADMIN_PASSWORD'/' $COUCHDB/local.ini && \
+[ "$ADMIN_USER" ] && [ "$ADMIN_PASSWORD" ] && \
+    sed -i -r 's/;admin = mysecretpassword/'$ADMIN_USER' = '$ADMIN_PASSWORD'/' $COUCHDB/local.ini && \
     echo "Apache CouchDB => admin account successfully created"
 
 # run CouchDB server
